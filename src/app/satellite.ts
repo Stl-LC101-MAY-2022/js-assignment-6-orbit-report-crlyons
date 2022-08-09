1c11aa83c41b6d346119cbc60c34b721bf03530f
Carl <cindymadewhat@gmail.com>
2022-08-08 20:13:08 -0500

    parts 1, 2, 3 complete

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,7 +5,7 @@ export class Satellite {
 	launchDate: string;
 	orbitType: string;
 	operational: boolean;
-
+	
 	constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
 		this.name = name;
 		this.type = type;
@@ -15,7 +15,9 @@ export class Satellite {
    }
 	
 	isSpaceDebris(): boolean {
+		if (this.type === 'Space Debris'){
 		return true;
+	} else return false;
    }
 
 }
